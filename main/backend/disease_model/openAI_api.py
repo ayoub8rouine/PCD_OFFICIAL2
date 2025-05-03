5aca47c599c80a95f3393fbd73261aee23e6a148
Si-Cat <adamarf02@gmail.com>
2025-05-03 16:27:15 +0100

    Update openAI_api.py

--- main/backend/disease_model/openAI_api.py
+++ main/backend/disease_model/openAI_api.py
@@ -3,6 +3,22 @@ import base64
 from openai import AzureOpenAI
 from configopenAI import AzureOpenAIConfig  # Make sure the file is named 'openAIconfig.py'
 
+#AzurOpenAIConfig definition create a file that have this class named configopenAI.py and change the parametre
+# class AzureOpenAIConfig:  
+#     def __init__(self):  
+#         self.endpoint = os.getenv("ENDPOINT_URL", "https://openai-api-pcd-sym.openai.azure.com/")  
+#         self.deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-mini")  
+#         self.subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "REPLACE_WITH_YOUR_KEY_VALUE_HERE")  
+      
+#     def get_endpoint(self):  
+#         return self.endpoint  
+  
+#     def get_deployment(self):  
+#         return self.deployment  
+  
+#     def get_subscription_key(self):  
+#         return self.subscription_key  
+
 # Initialize config
 config = AzureOpenAIConfig()
 
